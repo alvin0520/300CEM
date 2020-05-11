cf342b6198287397157fe5dfbd1f7f45a8e79e9c
Mo <197022316@stu.vtc.edu.hk>
2020-05-11 22:58:48 +0800

    Topic button

--- app/src/main/java/com/mo/a300cem/MainActivity.java
+++ app/src/main/java/com/mo/a300cem/MainActivity.java
@@ -4,21 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     String user;
@@ -47,23 +55,80 @@ public class MainActivity extends AppCompatActivity {
             //add button to the layout
             LL1.addView(addTopic);
         }
+        TextView Topic = new TextView(this);
+        Topic.setText("▼▼▼ All Topics ▼▼▼");
+        Topic.setTextSize(25);
+        Topic.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        LL1.addView(Topic);
 
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.1.226/getTopic.php";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
+
                     JSONObject jsonObj = new JSONObject(response);
+                    JSONArray jsonArrayTable = jsonObj.getJSONArray("topic");
 
+                    for (int i = 0; i < jsonArrayTable.length(); i++) {
+                        //add a divided line
+                        View line = new View(MainActivity.this);
+                        line.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 3));
+                        line.setBackgroundColor(Color.BLACK);
+                        LL1.addView(line);
+                        //create button with topic title
+                        Button btn = new Button(MainActivity.this);
+                        btn.setBackgroundColor(Color.WHITE);
+                        btn.setTextColor(Color.BLACK);
+                        JSONObject jsonObjRow = jsonArrayTable.getJSONObject(i);
+                        final String Cont = jsonObjRow.getString("content");
+                        final String date = jsonObjRow.getString("date");
+                        final String user = jsonObjRow.getString("user");
+                        btn.setText(jsonObjRow.getString("name"));
+                        btn.setId(jsonObjRow.getInt("id"));
+                        btn.setTextSize(14);
+                        final int id_ = btn.getId();
+                        LL1.addView(btn);
+                        btn = ((Button) findViewById(id_));
+                        btn.setHeight(22);
+                        //add onclicklistener for each button
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View view) {
+
+                                Intent i = new Intent(MainActivity.this, ViewCont.class);
+                                i.putExtra("id", id_);
+                                i.putExtra("FirstCont", Cont);
+                                i.putExtra("date", date);
+                                i.putExtra("user", user);
+                                startActivity(i);
+
+                            }
+                        });
+                    }
+                    View line = new View(MainActivity.this);
+                    line.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 3));
+                    line.setBackgroundColor(Color.BLACK);
+                    LL1.addView(line);
                 } catch (JSONException e) {
-
+                    TextView tV = new TextView(MainActivity.this);
+                    tV.setText(e.toString());
+                    LL1.addView(tV);
                 }
             }
-        };
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-        getTopic getTopic = new getTopic(responseListener);
-        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-        queue.add(getTopic);
+            }
+        });
 
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     // create an action bar button
---
+++ app/src/main/java/com/mo/a300cem/ViewCont.java
@@ -0,0 +1,21 @@
+package com.mo.a300cem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ViewCont extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cont);
+
+        Intent ReIntent = getIntent();
+
+
+
+
+    }
+}
